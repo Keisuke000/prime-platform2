20a9dd99d7eb9980a579e956adc9401e7a5209e8
keisuke <ramitsu.k11@gmail.com>
2023-08-22 14:37:12 +0900

    ローカルでビルドできるようにした Yes!

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !!!!!
+          Learn React !!!!! Yes!
         </a>
       </header>
       {/* <header /> */}
