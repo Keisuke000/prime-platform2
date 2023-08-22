6d1a78a1a3a8069b841989670c29287d82bb3ba8
keisuke <ramitsu.k11@gmail.com>
2023-08-22 13:50:00 +0900

    元に戻してみた

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './styles/sign-up.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,9 +23,9 @@ function App() {
         >
           Learn React !!!!!
         </a>
-      </header> */}
-      <header />
-      <sidebar />
+      </header>
+      {/* <header /> */}
+      {/* <sidebar /> */}
     </div>
   );
 }
