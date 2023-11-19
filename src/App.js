f3e2cb62c9a74a279478316113ecde5efce96f2b
keisuke <ramitsu.k11@gmail.com>
2023-11-19 15:37:47 +0900

    久々なのでとりあえずコミット。

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import Header from './header';
-import Sidebar from './sidebar';
+import Sidebar from './components/Sidebar';
 import Main_contents from './main_contents';
+import Main_contents2 from './main_contents2';
 import React from 'react';
 import Mwrapper from './mwrapper';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
 
+// import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function App() {
-  const subject = "React dayo"
-  console.log(subject);
-  return (    
+  return (
     <body>
       <div><Header /></div>
       <div><Sidebar /></div>
+      {/* <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path={`/`} element={<Main_contents />} />
+            <Route path={`/home/`} element={<Main_contents2 />} />
+            <Route path={`/analitics/`} element={<Main_contents2 />} />
+          </Routes>
+        </BrowserRouter>
+      </div> */}
       <div><Main_contents /></div>
       <div><Mwrapper /></div>
     </body>
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Icon from "../images/icon.png"
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      {/* <img src={Icon} /> */}
+      <img class="primen-logo" src="logos/primen-logo.webp" alt="primen-logo" />
+      <p>programming@gmail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
